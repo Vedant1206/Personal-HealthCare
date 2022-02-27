35b891cf3397d620a4861782332c8d72135f68f5
Vedant Pulahru <pulahruv@myumanitoba.ca>
2022-02-27 20:12:54 +0000

    Search bar

--- app/app/src/main/java/ca/umanitoba/personalhealthcare/SearchActivity.java
+++ app/app/src/main/java/ca/umanitoba/personalhealthcare/SearchActivity.java
@@ -2,18 +2,27 @@ package com.example.personal_health_care;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Toast;
+
 import androidx.appcompat.widget.SearchView;
 
 public class SearchActivity extends AppCompatActivity {
 
     ListView listView;
-    String[] name = {"Headache","Nausea","Stiff Neck","Muscle Pain","Fever, flue, Cold,", "Vomiting"};
+    LinearLayout linearL;
+    String[] name = {"Headache","Nausea","Fever, flue, Cold,"};
 
+    //"Headache","Nausea","Stiff Neck","Muscle Pain","Fever, flue, Cold,", "Vomiting"
     ArrayAdapter<String> arrayAdapter;
 
     @Override
@@ -21,11 +30,26 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         setTitle("Enter Sign/Symptom");
+
+        linearL = new LinearLayout(this);
         listView = findViewById(R.id.listview);
 
         arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, name);
         listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                //Toast.makeText(SearchActivity.this, ""+arrayAdapter.getPosition(position),
+                //      Toast.LENGTH_SHORT).show();
+                if(position == 0){
+                    Intent i = new Intent(SearchActivity.this, HeadacheActivity.class);
+                    startActivity(i);
+                }
+            }
+        });
     }
+
 
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.menu, menu);
@@ -33,6 +57,7 @@ public class SearchActivity extends AppCompatActivity {
 
         SearchView searchView = (SearchView) menuItem.getActionView();
         searchView.setQueryHint("Type here to search");
+
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             //once use types
