0702daa6563e94bf9a3fc4122154ef463e0cecf9
Sangil Han <hans34@myumanitoba.ca>
2022-02-27 16:17:40 -0600

    Testing Patient class

--- app/app/src/main/java/ca/umanitoba/personalhealthcare/Guest.java
+++ app/app/src/main/java/ca/umanitoba/personalhealthcare/Guest.java
@@ -1,9 +1,11 @@
 package com.example.personal_health_care;
 
 public class Guest extends User{
-    public User(String ID){
-        super(ID);
+    
+    public Guest(String ID) {
+        super(ID);        
     }
+    
     protected String getID(){return this.ID;}
     protected void setID(String ID){this.ID = ID;}
 }
