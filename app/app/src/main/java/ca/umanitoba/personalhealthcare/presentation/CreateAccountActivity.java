4231227d14d951d264753c9a2fb2173f0f8c5e2f
ImKevin <maingockien>
2022-03-17 16:59:27 -0500

    Implement Create Account Activity with create account feedback

--- app/app/src/main/java/ca/umanitoba/personalhealthcare/presentation/CreateAccountActivity.java
+++ app/app/src/main/java/ca/umanitoba/personalhealthcare/presentation/CreateAccountActivity.java
@@ -2,33 +2,85 @@ package ca.umanitoba.personalhealthcare.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
-
-import java.util.ArrayList;
+import android.widget.Toast;
 
 import ca.umanitoba.personalhealthcare.R;
+import ca.umanitoba.personalhealthcare.business.AccountManager;
+import ca.umanitoba.personalhealthcare.business.AccountManagerImp;
+import ca.umanitoba.personalhealthcare.objects.EmailExistException;
+import ca.umanitoba.personalhealthcare.objects.EmailInvalidException;
+import ca.umanitoba.personalhealthcare.objects.Member;
+import ca.umanitoba.personalhealthcare.objects.PasswordInvalidException;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
-    ArrayList<String> emails = new ArrayList<String>();
-    ArrayList<String> passwords = new ArrayList<String>();
-    int index = 0;
+    AccountManager accountManager;
+
+    EditText emailInput;
+    EditText passwordInput;
+
+    TextView createFeedback;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        accountManager = new AccountManagerImp();
+
+        emailInput = findViewById(R.id.emailInput);
+        passwordInput = findViewById(R.id.passwordInput);
+        createFeedback = (TextView) findViewById(R.id.createAccountFeedback);
+        restartView();
     }
 
-    public void enter(View v){
-        TextView email = findViewById(R.id.email);
-        TextView password = findViewById(R.id.pass);
-        String em = email.getText().toString();
-        String pass = password.getText().toString();
-        Log.d("email", em);
-        Log.d("password",pass);
+    private void restartView () {
+        createFeedback.setVisibility(View.GONE);
+
+    }
+
+    public void onClickCreateAccount(View v) {
+        restartView();
+
+        String email = emailInput.getText().toString();
+        String password = passwordInput.getText().toString();
+
+        try {
+            Member newMember = accountManager.createAccount(email, password);
+        } catch (PasswordInvalidException e) {
+            notifyPasswordInvalid(e);
+        } catch (EmailInvalidException e) {
+            notifyEmailInvalid(e);
+
+        } catch (EmailExistException e) {
+            notifyEmailExist(e);
+        }
+        //TODO: Succeed creating .. do something afterward
+    }
+
+    void feedbackException(Exception e) {
+        createFeedback.setText(e.getMessage());
+        createFeedback.setVisibility(View.VISIBLE);
+        createFeedback.setTextColor(Color.RED);
+    }
+
+    void notifyEmailInvalid(EmailInvalidException e) {
+        feedbackException(e);
+    }
+
+    void notifyPasswordInvalid(PasswordInvalidException e) {
+        feedbackException(e);
+
+    }
+
+    void notifyEmailExist(EmailExistException e) {
+        feedbackException(e);
 
     }
 }
