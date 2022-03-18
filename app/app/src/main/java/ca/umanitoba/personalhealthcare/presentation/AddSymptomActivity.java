f3e99ea6d6ec3b5c949b96bb45208f7fb7a86d9c
Adam Kroeker <kroeke30@myumanitoba.ca>
2022-03-18 01:53:13 -0500

    Added Add a Symptom UI

---
+++ app/app/src/main/java/ca/umanitoba/personalhealthcare/presentation/AddSymptomActivity.java
@@ -0,0 +1,14 @@
+package ca.umanitoba.personalhealthcare.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddSymptomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_symptom);
+    }
+}
