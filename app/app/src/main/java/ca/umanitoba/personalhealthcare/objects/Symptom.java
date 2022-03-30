baff199ee765647031e445408aec2309ba2b15b6
Adam Kroeker <kroeke30@myumanitoba.ca>
2022-03-30 17:56:50 -0500

    reportSymptom

--- app/app/src/main/java/ca/umanitoba/personalhealthcare/objects/Symptom.java
+++ app/app/src/main/java/ca/umanitoba/personalhealthcare/objects/Symptom.java
@@ -1,17 +1,30 @@
 package ca.umanitoba.personalhealthcare.objects;
 
+import java.util.ArrayList;
+
 public class Symptom {
     String name;
     String description;
+    ArrayList<String> affectedBodyParts;
+    ArrayList<String> associatedConditions;
 
     public Symptom(String name, String description){
         this.name = name;
         this.description = description;
     }
-    public String getDescription() {
-        return description;
+
+    public Symptom(String name, ArrayList<String> affectedBodyParts, ArrayList<String> associatedConditions){
+        this.name = name;
+        this.affectedBodyParts = affectedBodyParts;
+        this.associatedConditions = associatedConditions;
     }
-    public String getSymptomName() {
-        return name;
-    }
+
+    public String toString() {return name + ":\nAffected body parts: " + affectedBodyParts.toString()
+            + "\nAssociated conditions: " + associatedConditions.toString() + "\n";}
+    public boolean addAffectedBodyPart(String bodyPart){return affectedBodyParts.add(bodyPart);}
+    public boolean addAssociatedCondition(String condition){return associatedConditions.add(condition);}
+    public String getDescription() {return description;}
+    public String getSymptomName() {return name;}
+    public ArrayList<String> getAffectedBodyParts() {return affectedBodyParts;}
+    public ArrayList<String> getAssociatedConditions() {return associatedConditions;}
 }
