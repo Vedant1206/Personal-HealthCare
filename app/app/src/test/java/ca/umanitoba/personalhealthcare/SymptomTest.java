c754edbe95805bcb21a4f4d08d823ed15cd65eb4
Asit Patel <patela8@myumanitoba.ca>
2022-04-01 17:12:29 -0500

    finished Symptom object unit testing

--- app/app/src/test/java/ca/umanitoba/personalhealthcare/SymptomTest.java
+++ app/app/src/test/java/ca/umanitoba/personalhealthcare/SymptomTest.java
@@ -1,8 +1,6 @@
 package ca.umanitoba.personalhealthcare;
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import ca.umanitoba.personalhealthcare.objects.Symptom;
@@ -22,7 +20,22 @@ public class SymptomTest {
         assertTrue("Symptom toString() should return its name", "Blurry vision".equals(sym) );
 
 
-        System.out.println("Finished testSC");
+        System.out.println("Finished testCreation");
+    }
+
+    @Test
+    public void testEquals(){
+        System.out.println("\nStarting Symptom equals() test");
+
+        Symptom sym1 = new Symptom("Blurry vision", "Head");
+        Symptom sym2 = new Symptom("Blurry vision", "Legs");
+        Symptom sym3 = new Symptom("Headache", "Head");
+
+        assertTrue("Checking if Blurry vision equals Blurry vision", sym1.equals(new Symptom("Blurry vision", "Head")) );
+        assertFalse("Both symptoms bodyParts are different, should not be equal", sym2.equals(sym1) );
+        assertFalse("Both symptoms names are different, should not be equal", sym1.equals(sym3) );
+
+        System.out.println("Finished testEquals");
     }
 
 }
