8f584be35aa2e3b98320c565c0bc75d94acc5f49
ImKevin <maingockien>
2022-04-23 20:48:22 -0500

    Refactor Member Persistence Unit Test

--- app/app/src/test/java/ca/umanitoba/personalhealthcare/persistence/MemberPersistenceTest.java
+++ app/app/src/test/java/ca/umanitoba/personalhealthcare/persistence/MemberPersistenceTest.java
@@ -1,5 +1,8 @@
 package ca.umanitoba.personalhealthcare.persistence;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,29 +15,51 @@ import ca.umanitoba.personalhealthcare.objects.Patient;
 import ca.umanitoba.personalhealthcare.persistence.fakeDb.FakeMemberPersistence;
 
 public class MemberPersistenceTest {
+    private static final int SAMPLE_SIZE = 10;
     MemberPersistence persistence;
+    Member[] sampleMembers;
+    String email;
+    String password;
 
     @Before
     public void setup () {
+        email = "random@email.com";
+        password = "P@ssword123!";
+
         persistence = FakeMemberPersistence.getMemberPersistence();
+
+        sampleMembers = new Member[SAMPLE_SIZE];
+        for (int i = 0; i < SAMPLE_SIZE; i++) {
+            try {
+                sampleMembers[i] = new Patient(i+"", i+email, password);
+            } catch (EmailInvalidException e) {
+                assertTrue(false);
+            } catch (PasswordInvalidException e) {
+                assertTrue(false);
+            }
+            try {
+                persistence.createMember(sampleMembers[i]);
+            } catch (EmailExistException e) {
+                e.printStackTrace();
+                assertTrue(false);
+            }
+        }
+
+
+    }
+
+    @After
+    public void clean () {
+
     }
 
     @Test
-    public void testCreateMember () {
+    public void testGetMember () {
 
-        try {
-            persistence.createMember(new Patient("1", "randomEmail@gmail.com", "P@ssword123!"));
-        } catch (EmailExistException e) {
-            e.printStackTrace();
-        } catch (EmailInvalidException e) {
-            e.printStackTrace();
-            return;
-        } catch (PasswordInvalidException e) {
-            e.printStackTrace();
-            return;
+        for (int i = 0; i < SAMPLE_SIZE; i++) {
+            String emailToGet = sampleMembers[i].getEmail();
+            Member member = persistence.getMember(emailToGet, password);
+            Assert.assertNotNull(member);
         }
-
-        Member member = persistence.getMember("randomEmail@gmail.com", "P@ssword123!");
-        Assert.assertNotNull(member);
     }
 }
