fb0dafc668fecad33081313ffc4bc41237ead806
Adam Kroeker <kroeke30@myumanitoba.ca>
2022-04-24 19:25:00 -0500

    added a bunch of comments

--- app/app/src/main/java/ca/umanitoba/personalhealthcare/business/ProfileManagerImp.java
+++ app/app/src/main/java/ca/umanitoba/personalhealthcare/business/ProfileManagerImp.java
@@ -8,16 +8,10 @@ import ca.umanitoba.personalhealthcare.objects.Profile;
 import java.util.List;
 
 import ca.umanitoba.personalhealthcare.persistence.ProfilePersistence;
-
-//import ca.umanitoba.personalhealthcare.business.ProfileManager;
-//import ca.umanitoba.personalhealthcare.persistence.fakeDb.FakeMemberPersistence;
 import ca.umanitoba.personalhealthcare.objects.NameExistsException;
 
 public class ProfileManagerImp implements ProfileManager{
-    private ProfilePersistence profilePersistence;
-//    private Member selectedMember;
-//
-//    private MemberPersistence members;
+    private ProfilePersistence profilePersistence;  //Profile persistence
 
     /**
      * The constructor for ProfileMannagerImp
@@ -28,6 +22,19 @@ public class ProfileManagerImp implements ProfileManager{
         this.profilePersistence = profilePersistence;
     }
 
+    /**
+     * insertProfile
+     * @param email String
+     * @param name String
+     * @param address String
+     * @param height int
+     * @param weight int
+     * @param year int
+     * @param month int
+     * @param day int
+     * @param sex String
+     * @return Profile
+     */
     public Profile insertProfile(String email, String name, String address,
                                  int height, int weight,
                                  int year, int month, int day,
@@ -37,25 +44,51 @@ public class ProfileManagerImp implements ProfileManager{
         return newProfile;
     }
 
+    /**
+     * deleteProfile
+     * @param profileDeleting Profile object
+     */
     public void deleteProfile(Profile profileDeleting){
         profilePersistence.deleteProfile(profileDeleting);
     }
 
+    /**
+     * getProfiles
+     * @param email String
+     * @return List
+     */
     public List<Profile> getProfiles(String email){
         List<Profile> lookingFor = profilePersistence.getProfiles(email);
         return lookingFor;
     }
 
+    /**
+     * updateProfile
+     * @param newProfile Profile object
+     * @return Profile
+     */
     public Profile updateProfile(Profile newProfile){
         return profilePersistence.updateProfile(newProfile);
     }
 
+    /**
+     * getProfile
+     * @param email String
+     * @param profileName String
+     * @return Profile
+     */
     @Override
     public Profile getProfile(String email, String profileName) {
 
         return profilePersistence.getProfile(email, profileName);
     }
 
+    /**
+     * updateProfileName
+     * @param initName String
+     * @param profile Profile object
+     * @return Profile
+     */
     @Override
     public Profile updateProfileName(Profile profile, String initName) throws NameExistsException {
         return profilePersistence.updateProfileName(profile, initName);
